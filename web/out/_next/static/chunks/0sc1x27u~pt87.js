4960c5e89cf7a37efd879b44594b0fb364656162
Kerim Hestnes Nisancioglu <kerim@uib.no>
2026-06-01 23:11:26 +0200

    Delete web directory

--- web/out/_next/static/chunks/0sc1x27u~pt87.js
+++
@@ -1,1 +0,0 @@
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,24764,t=>{t.v(c=>Promise.all(["static/chunks/11gd9ct8y_ry~.js"].map(c=>t.l(c))).then(()=>c(66667)))}]);
